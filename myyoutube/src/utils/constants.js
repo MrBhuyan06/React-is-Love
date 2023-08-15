7f2a6b509a514f8889055c9618256cfdbda9c6bc
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-08-15 20:36:05 +0530

    added the constat data

---
+++ myyoutube/src/utils/constants.js
@@ -0,0 +1,3 @@
+const API_KEY = "AIzaSyDQy93BfrmmqLu4P8ouvWpiJjQVshghIxM";
+export const YOUTUBE_VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyDQy93BfrmmqLu4P8ouvWpiJjQVshghIxM";
