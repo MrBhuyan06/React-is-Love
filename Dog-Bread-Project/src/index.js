afaf4552e832d405dd7c81198304935198a6ee59
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-09-01 11:18:08 +0530

    Added the init setup file

---
+++ Dog-Bread-Project/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client.js";
+
+const APP = () => {
+  return (
+    <>
+      <h1>REACT IS RUNNING</h1>
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<APP />);
